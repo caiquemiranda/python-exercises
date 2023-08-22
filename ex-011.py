0127e38aeb3f939140f1ff1eae5fd8bd474f9518
caiquemiranda <caiquemirandas@gmail.com>
2023-08-21 21:03:55 -0300

    code-11-15

--- ex-011.py
+++ ex-011.py
@@ -1,2 +1,12 @@
+# Calcule o resultado da expressão A > B and C or D, utilizando os valores da tabela a seguir.
 
+"""
+A B C D Resultado
+1 2 True False
+10 3 False False
+5 1 True True
+"""
 
+# False
+# False
+# True
--- ex-012.py
+++ ex-012.py
@@ -1,2 +1,12 @@
+"""
+Escreva uma expressão que será utilizada para decidir se um aluno foi ou não aprovado. Para ser
+aprovado, todas as médias do aluno devem ser maiores que 7. Considere que o aluno cursa
+apenas três matérias, e que a nota de cada uma está armazenada nas seguintes variáveis:
+matéria1, matéria2 e matéria3.
+"""
 
+# Pelo enunciado:
+#matéria1 > 7 and matéria2 > 7 and matéria3 > 7
 
+# Na prática, o aluno é aprovado se obtiver nota maior ou igual a média, logo:
+#matéria1 >= 7 and matéria2 >= 7 and matéria3 >= 7
